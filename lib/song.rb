d18c0c06be351e504aa1e6e106b5cab9bcbd1fc0
Komal Dindiyal <komald.4@gmail.com>
2020-01-31 22:08:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,7 @@
     @@count += 1
   end
   attr_accessor :name, :artist, :genre
-end
-end
+
 
 the_day = Song.new("The Day", "John Smith", "classic")
 tomorow = Song.new("Tomorow", "John Smith", "classic")
